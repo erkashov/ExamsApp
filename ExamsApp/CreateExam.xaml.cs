5a6188bfa1d31c27d28e97417e26584b431d5f6b
erkashov <erkashov@DESKTOP-L8LHNVB>
2021-10-23 16:48:00 +0300

    Добавьте файлы проекта.

---
+++ ExamsApp/CreateExam.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ExamsApp
+{
+    /// <summary>
+    /// Логика взаимодействия для CreateExam.xaml
+    /// </summary>
+    public partial class CreateExam : Window
+    {
+        private Exam cur_ex;
+        public CreateExam(Exam ex)
+        {
+            InitializeComponent();
+            cur_ex = ex;
+            if(ex!=null)lbQues.ItemsSource = ExamDBEntities.GetContext().Exams.Find(cur_ex.Id).Questions.ToList();
+        }
+
+        private void addQues_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void start_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ExamsApp/Windows/AdminWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ExamsApp.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminWindow.xaml
+    /// </summary>
+    public partial class AdminWindow : Window
+    {
+        private User cur_user;
+        public AdminWindow(User u)
+        {
+            InitializeComponent();
+            cur_user = u;
+        }
+
+        private void createExam_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
